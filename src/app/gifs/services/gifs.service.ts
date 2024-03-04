893f1d89ee850488ec0700b9ab8bfbbabfd798ee
Pablo Alejandro Sangenis <pasangenis@teco.com.ar>
2024-03-04 20:15:24 -0300

    gifService completed

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,18 +1,48 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({ providedIn: 'root' })
 export class GifsService {
-  private _tagHistory: string[] = [];
+  public gifList: Gif[] = [];
 
-  constructor() {}
+  private _tagHistory: string[] = [];
+  private apiKey: string = '2MlHV8O0vKTC0r91MG8ed3RT7gdkgk7C';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
+
+  constructor(private http: HttpClient) {}
 
   get tagHistory() {
     return [...this._tagHistory];
   }
 
-  public searchTag(tag: string): void {
-    console.log(tag)
+  private organizeHistory(tag: string) {
+    tag = tag.toLowerCase();
+    if (this._tagHistory.includes(tag)) {
+      this._tagHistory = this._tagHistory.filter((oldTag) => oldTag !== tag);
+    }
     this._tagHistory.unshift(tag);
-    console.log(this.tagHistory)
+    this._tagHistory = this._tagHistory.splice(0, 10);
+  }
+
+  searchTag(tag: string): void {
+    if (tag.length === 0) return;
+    this.organizeHistory(tag);
+    // Esta es la forma tradicional, pero FH no lo hace así, usa HttpClient
+    // fetch(
+    //   'https://api.giphy.com/v1/gifs/trending?api_key=2MlHV8O0vKTC0r91MG8ed3RT7gdkgk7C&limit=10'
+    // )
+    //   .then((resp) => resp.json())
+    //   .then((data) => console.log(data));
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', tag);
+    this.http
+      .get<SearchResponse>(`${this.serviceUrl}/search`, { params })
+      .subscribe((resp) => {
+        this.gifList = resp.data;
+      });
   }
 }
